a8caf24757f953b806fe8110a9d8ab93de3da9c8
Sebastian Glaß <sglass@hs-mittweida.de>
2026-05-17 03:36:01 +0200

    feat: add powermenu with type-3 layout

---
+++ hypr/scripts/powermenu.sh
@@ -0,0 +1,53 @@
+#!/usr/bin/env bash
+
+theme="$HOME/.config/rofi/powermenu.rasi"
+
+lock="󰌾"
+logout="󰍃"
+suspend="󰒲"
+hibernate="󰴻"
+reboot="󰜉"
+shutdown="󰐥"
+yes="󰄬"
+no="󰅖"
+
+rofi_cmd() {
+    rofi -dmenu -theme "$theme"
+}
+
+confirm_cmd() {
+    rofi \
+        -theme-str 'window {location: center; anchor: center; width: 200px; x-offset: 0; y-offset: 0;}' \
+        -theme-str 'mainbox {children: ["message","listview"]; spacing: 8px; padding: 10px;}' \
+        -theme-str 'listview {columns: 2; lines: 1; spacing: 8px; fixed-columns: true;}' \
+        -theme-str 'element-text {horizontal-align: 0.5; font: "JetBrainsMono Nerd Font 18";}' \
+        -theme-str 'message {padding: 8px; border-radius: 5px;}' \
+        -theme-str 'textbox {horizontal-align: 0.5;}' \
+        -dmenu \
+        -mesg "Are you sure?" \
+        -theme "$theme"
+}
+
+confirm_run() {
+    [[ "$(echo -e "$yes\n$no" | confirm_cmd)" == "$yes" ]] && $@
+}
+
+selected=$(echo -e "$lock\n$logout\n$suspend\n$hibernate\n$reboot\n$shutdown" | rofi_cmd)
+
+export HYPRLAND_INSTANCE_SIGNATURE=$(
+    for dir in "$XDG_RUNTIME_DIR/hypr/"/*/; do
+        if [ -S "${dir}.socket.sock" ]; then
+            basename "$dir"
+            break
+        fi
+    done
+)
+
+case "$selected" in
+    "$lock")      hyprlock ;;
+    "$logout")    confirm_run hyprctl dispatch "hl.dsp.exec_cmd(\"hyprshutdown -t 'Logging out...' --post-cmd 'uwsm stop'\")" ;;
+    "$suspend")   confirm_run systemctl suspend ;;
+    "$hibernate") confirm_run systemctl hibernate ;;
+    "$reboot")    confirm_run hyprctl dispatch "hl.dsp.exec_cmd(\"hyprshutdown -t 'Rebooting...' --post-cmd 'systemctl reboot'\")" ;;
+    "$shutdown")  confirm_run hyprctl dispatch "hl.dsp.exec_cmd(\"hyprshutdown -t 'Shutting down...' --post-cmd 'systemctl poweroff'\")" ;;
+esac
